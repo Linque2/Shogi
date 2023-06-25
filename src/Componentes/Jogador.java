89d6e6c0ccdca1185c10d60427267a5e97c20682
gustavoamaraal <127237899+gustavoamaraal@users.noreply.github.com>
2023-06-24 22:00:43 -0300

    enviando jogador, gyokushou e oushou corretos

--- src/Componentes/Jogador.java
+++ src/Componentes/Jogador.java
@@ -1,7 +1,5 @@
 /**
- * Criado por Rafael Masato Haga Costa em 22/06/2023
  * TODO: definir as subclasses de jogadores (um que contém o 玉将-Rei de joias- e outro que possui o 王将-Rei-), na classe do primeiro, adicionar o metodo Furigoma
- *    
  */
 
 package Componentes;
