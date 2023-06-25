7470f8a8e3fc36070b06e9423541eb18579d2622
Rafael Masato <rmcosta194@gmail.com>
2023-06-25 15:38:44 -0300

    Corrigidas as coordenadas

--- src/Componentes/SentidosFundamentais.java
+++ src/Componentes/SentidosFundamentais.java
@@ -2,16 +2,16 @@ package Componentes;
 
 public enum SentidosFundamentais {
     
-    NORTE(new Coordenada(0,1)),
-    SUL(new Coordenada(0,-1)),
-    LESTE(new Coordenada(1,0)),
-    OESTE(new Coordenada(-1, 0)),
-    NE(new Coordenada(1, 1)),
-    SE(new Coordenada(1,-1)),
-    NO(new Coordenada(-1, 1)),
-    SO(new Coordenada(-1, -1)),
-    L_NE(new Coordenada(1,2)),
-    L_NO(new Coordenada(-1, 2));
+    NORTE(new Coordenada(0,-1)),
+    SUL(new Coordenada(0,1)),
+    LESTE(new Coordenada(-1,0)),
+    OESTE(new Coordenada(1, 0)),
+    NE(new Coordenada(1, -1)),
+    SE(new Coordenada(1,1)),
+    NO(new Coordenada(-1, -1)),
+    SO(new Coordenada(-1, 1)),
+    L_NE(new Coordenada(1,-2)),
+    L_NO(new Coordenada(-1, -2));
 
     private final Coordenada vetor;
 
